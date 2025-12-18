2d29303b53b2e862497350cff43eab41fe71b2c2
dosamani <rleggs@gmail.com>
2025-12-18 09:48:18 -0600

    Set static URL path for Flask app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import os
 
-app = Flask(__name__, static_folder="static", static_url_path="")
+app = Flask(__name__, static_folder="static", static_url_path="/static")
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # ----------------------------
